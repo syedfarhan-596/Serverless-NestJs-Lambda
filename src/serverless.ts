f8bfad0a3776b4ab53b439ade8726e9dc1f89d1e
Syed Farhan <syedfarhan596@gmail.com>
2025-07-15 08:29:14 +0530

    start the project

---
+++ src/serverless.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Context, Callback, APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import { Express } from 'express';
+
+let cachedServer: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance() as Express;
+
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback,
+): Promise<any> => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return cachedServer(event, context, callback);
+};
